ff7c12b0bac39414d2fb95313196cfeccd47ce34
Erin <emmulcahey@knox.edu>
2025-02-04 12:00:30 -0600

    On startup a random sign appears on screen. Barebones of picking and displaying random signs implemented

--- simon-signs/main.gd
+++ simon-signs/main.gd
@@ -1,12 +1,7 @@
 extends Node
 
-
-var current_sign = null
-var current_letter_index: int = -1
-
-func _get_new_question():
-	var prompt = $question.get_prompt
-	current_sign = prompt
+func _ready() -> void:
+	pass
 	
 
 #Code adapted from Joe Bustamante: "Godot Typing Game Tutorial"
@@ -15,10 +10,11 @@ func _unhandled_input(event : InputEvent) -> void:
 		var typed_event = event as InputEventKey
 		var key_typed = PackedByteArray([typed_event.unicode]).get_string_from_utf8()
 		
+		#if current_sign == key_typed (look up string comparsion in GDScript)
+			#correct answer function
+		#if timeer event zero
+			#time up
+			#incorrect answer function
+		# current_sign != key_typed
+			#incorrect answer function
 		
-		if current_sign == null:
-			_get_new_question()
-		else:
-			var prompt = current_sign.get_prompt()
-			if prompt.substr(0, 1) == key_typed:
-				print("yahoo")
