7bbe8812aa2f98d0848c021de067201ef27a3f21
meherahmad <147357392+meherahmad@users.noreply.github.com>
2025-02-11 20:15:26 -0600

    Lives_sound

--- simon-signs/Assets/lives-coins.gd
+++ simon-signs/Assets/lives-coins.gd
@@ -1,26 +1,33 @@
 extends Node
 
 @export
-var lives = 4 # Start with 6 lives
-@onready var heartBox = $HBoxContainer
-@onready var hearts = heartBox.get_children()  # Get reference to heart UI
+var lives = 4 # Start with 4 lives
+@onready var HeartContainer = $HBoxContainer
+@onready var hearts = HeartContainer.get_children()  # Get reference to heart UI
 @onready var heart1 = $HBoxContainer/Control/heart1
 @onready var heart2= $HBoxContainer/Control/heart2
 @onready var heart3= $HBoxContainer/Control/heart3
 @onready var heart4= $HBoxContainer/Control/heart4
 
+#sound effects
+@onready var wrong = $WrongAnswerPlayer
+@onready var right= $RightAnswerPlayer
 #$HBoxContainer/Control2/heart2
-#func _ready():
-	#update_hearts()  # Update UI when game starts
+func _ready():
+	update_hearts()  # Update UI when game starts
 
 func lose_life():
 	if lives > 0:
 		lives -= 1
 		update_hearts()
+		wrong.play() #produces the sound effect for a wrong answer
 		#heart4.visible= not heart4.visible
 	
 	if lives == 0:
 		game_over()
+		
+func right_ans():
+	right.play() # produces the sound effect for a right answer
 
 func update_hearts():
 	# Show only the number of hearts equal to lives
@@ -36,7 +43,9 @@ func game_over():
 func _input(event):
 	# Simulating a wrong answer with SPACE key
 	if event.is_action_pressed("ui_accept"):  # Default is SPACE key
-		lose_life() 	
+		lose_life()
+	elif event.is_action_pressed("ui_right"):  #if correct answer is entered
+		right_ans()
 
  #Called when the node enters the scene tree for the first time.
 #	pass # Replace with function body.
@@ -54,5 +63,5 @@ func _input(event):
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+#func _process(delta: float) -> void:
 	pass
