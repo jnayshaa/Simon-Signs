f25ad5fb7c5baf18450aef01ab10bf0eb59ff9b7
Dibyasha Sharma <dibyasha11@gmail.com>
2025-02-17 11:50:19 -0600

    Update buttons for flipbook

--- simon-signs/Menu.gd
+++ simon-signs/Menu.gd
@@ -16,3 +16,7 @@ func _on_learn_pressed():
 
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://credits.tscn") 
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://ui.tscn") 
