095b467a0d3806436edbc940cbf9fa8b8b9b2a26
Naysha <njain@knox.edu>
2025-03-04 12:21:16 -0600

    cleaning up (no code change)

--- simon-signs/resume_button.gd
+++ simon-signs/resume_button.gd
@@ -2,27 +2,18 @@ extends Button
 @onready var PauseMenu = $"../../../../.."
 @onready var lives= $Control
 
-func _ready():
-	print("Global is accessible:", Global)
+#func _ready():
+	#print("Global is accessible:", Global)
 
 func _on_resume_pressed():
 	PauseMenu.visible=false
 	get_tree().paused=false
-	#get_tree().change_scene_to_file("res://ui.tscn")
-
 
 func _on_quit_button_pressed():
 	get_tree().paused=false
 	get_tree().change_scene_to_file("res://startmenu.tscn")
 
-#func _on_sound_toggle_toggled(toggled_on):
-	#PauseMenu.visible=false
-	#get_tree().paused=false
-	#print("Calling Global.toggle_sound()...")
-	#Global.toggle_sound()
-
 func _on_sound_toggle_pressed():
 	PauseMenu.visible=false
 	get_tree().paused=false
-	print("Calling Global.toggle_sound()...")
 	Global.toggle_sound()
