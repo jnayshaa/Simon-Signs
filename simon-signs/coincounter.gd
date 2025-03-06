744c2b208fba06726f7b8bd5e2c0da1b7568150f
Dibyasha Sharma <dibyasha11@gmail.com>
2025-03-06 12:18:27 -0600

    Hint Label Updated

--- simon-signs/coincounter.gd
+++ simon-signs/coincounter.gd
@@ -3,6 +3,7 @@ extends Node
 @onready var gainedcoins = 0
 @onready var coins2=$".."
 @onready var label=$"../../UI/HintButtonPanel"
+@onready var warning = $"../../UI/CoinWarning"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -19,5 +20,9 @@ func _on_hint_button_pressed():
 	print ("Hint button pressed")
 	if label.is_visible() == false:
 		coins2._decrease_coin_amount()
+	if gainedcoins < 10 && label.is_visible() == false:
+			warning.set_visible(true)
+			await get_tree().create_timer(1.5).timeout
+			warning.set_visible(false)
 	#if gainedcoins>=10:
 		#label.visible=true
